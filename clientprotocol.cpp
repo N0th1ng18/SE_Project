00e20cf411ada9d27e825b15f90994f8aeb9c219
iTheHamburglar <47202018+iTheHamburglar@users.noreply.github.com>
2019-04-17 21:38:19 -0500

    Add files via upload

--- clientprotocol.cpp
+++ clientprotocol.cpp
@@ -16,14 +16,30 @@ void ClientProtocol::connectMainServer(QObject *parent)
 {
     qDebug() << "connectMainServer()";
     QByteArray data;
-    data = "Fuck You";
+    data = "Hello World";
     QTcpSocket *tcpSocket = new QTcpSocket(parent);
-    tcpSocket->connectToHost("192.168.1.109", 5555);
-    tcpSocket->write(data);
+    tcpSocket->connectToHost(QHostAddress::LocalHost, 1234);
+
+    if(!tcpSocket->waitForConnected(5000)){
+        qDebug() << "Failed to Connect to main server" << endl;
+    }
+    else{
+
+        tcpSocket->write(data);
+        tcpSocket->flush();
+    }
 
 }
 
 void ClientProtocol::disconnectMainServer()
 {
+    /*
+     * Socket->disconnectFromHost();
+     * if socket->state() == Qabstractsocket::unnconnectedstate ||
+     *  socket->waitfordisconnect(1000)){
+     *      qDebug() << "Disconnected";
+     * }
+
+    */
     qDebug() << "disconnectMainServer()";
 }
